ad963c0a590be507fa9d1f53534c6adde82cd783
Chris H <crhowell1@gmail.com>
2016-11-04 15:05:02 -0700

    Fix profiles urls.py to proper slug style regex

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^(?P<slug>\w+)/edit/$', views.EditProfile.as_view(), name='edit_profile'),
-    url(r'^(?P<slug>\w+)/$', views.ShowProfile.as_view(), name='show_profile'),
+    url(r'^(?P<slug>[-\w]+)/edit/$', views.EditProfile.as_view(), name='edit_profile'),
+    url(r'^(?P<slug>[-\w]+)/$', views.ShowProfile.as_view(), name='show_profile'),
 ]
